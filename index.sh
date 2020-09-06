b8aefec4de193806a1020fd201d3e7f0a5ac58c4
Mimi <caikinhdep003@gmail.com>
2020-09-07 00:00:00 +0700

    dummy commit

---
+++ index.sh
@@ -0,0 +1,4 @@
+./draw_github_contributions.sh 1.txt     0 1
+./draw_github_contributions.sh 2.txt     0 2
+./draw_github_contributions.sh 3.txt     0 3
+./draw_github_contributions.sh 4.txt     0 4
